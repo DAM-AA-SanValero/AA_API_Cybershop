38fae726992f9caa6dbe245e1153a69777102039
Joserra2304 <joserra2304@gmail.com>
2023-01-02 16:31:42 +0100

    Proyecto inicial

---
+++ src/main/java/com/svalero/cybershop/CybershopApplication.java
@@ -0,0 +1,13 @@
+package com.svalero.cybershop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CybershopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CybershopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/svalero/cybershop/CybershopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svalero.cybershop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CybershopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
