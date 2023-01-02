0f831e72b2c90c77347c09fbb9a70431b2e62fd8
Joserra2304 <joserra2304@gmail.com>
2023-01-02 18:35:51 +0100

    Crear clases y atributos, con sus restricciones y validaciones

---
+++ src/main/java/com/svalero/cybershop/domain/Client.java
@@ -0,0 +1,45 @@
+package com.svalero.cybershop.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name= "client")
+public class Client {
+
+       @Id
+       @GeneratedValue(strategy = GenerationType.IDENTITY)
+       private long id;
+
+       @Column
+       @NotBlank
+       @NotNull
+       private String name;
+
+       @Column
+       @NotBlank
+       @NotNull
+       private String surname;
+
+       @Column
+       @Size(max=9)
+       @PositiveOrZero
+       private int mobileNumber;
+
+       @Column
+       @PastOrPresent
+       private LocalDate registerDate;
+
+       @Column
+       private boolean isVIP;
+
+
+}
